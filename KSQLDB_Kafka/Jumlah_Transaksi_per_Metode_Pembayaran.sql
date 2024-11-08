88b24c0b48d320730e8bd8e2f0d5dc8812e9b96c
Dhoifullah Luth Majied <107235147+Ajied21@users.noreply.github.com>
2024-11-08 16:49:11 +0700

    Delete KSQLDB_Kafka directory

--- KSQLDB_Kafka/Jumlah_Transaksi_per_Metode_Pembayaran.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE payment_method_counts AS
-SELECT 
-    payment_method,
-    COUNT(*) AS transaction_count
-FROM 
-    Data_Transaksi_Keuangan_E_commerce
-GROUP BY 
-    payment_method
-EMIT CHANGES;
--- KSQLDB_Kafka/Rata_rata_Usia_Pelanggan_per_Kategori_Produk.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE avg_age_per_category AS
-SELECT 
-    product_category,
-    AVG(age) AS average_age
-FROM 
-    Data_Transaksi_Keuangan_E_commerce
-GROUP BY 
-    product_category
-EMIT CHANGES;
